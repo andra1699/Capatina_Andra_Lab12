4483718d6645b4f6d3aba45959a448f6016aca4f
ANDRA-CAMELIA CAPATINA <capatina.andra163321@student.ubbcluj.ro>
2022-01-16 19:18:28 +0200

    Add project files.

---
+++ Capatina_Andra_Lab12/Capatina_Andra_Lab12/Capatina_Andra_Lab12/Data/IRestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Capatina_Andra_Lab12.Models;
+using Capatina_Andra_Lab12.Data;
+
+
+namespace Capatina_Andra_Lab12
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
---
+++ WebAPI/Data/WebAPIContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPI.Models.ShopList> ShopList { get; set; }
+    }
